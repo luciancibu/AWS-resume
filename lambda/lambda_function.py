af7bcf05f8a7f64d502cc121e1b92669de1e5fc4
Lucian Cibu <luciancibu@yahoo.com>
2025-12-05 18:14:04 +0200

    Small updates

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -4,6 +4,17 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('resume-lucian-cibu')
 
 def lambda_handler(event, context):
+
+    if event.get("queryStringParameters", {}).get("read") == "true":
+        response = table.get_item(Key={'id': '0'})
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": str(response["Item"]["views"])
+        }
+
     response = table.get_item(Key={'id': '0'})
     views = response['Item']['views'] + 1
     table.put_item(Item={'id': '0', 'views': views})
@@ -11,9 +22,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "headers": {
-            "Access-Control-Allow-Origin": "*",   
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Methods": "GET"
+            "Access-Control-Allow-Origin": "*"
         },
         "body": str(views)
     }
