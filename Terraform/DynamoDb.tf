320bf342f0feae65e3f123b7101f74a629462afa
Lucian Cibu <luciancibu@yahoo.com>
2025-12-07 01:04:17 +0200

    Update GitHub Actions

--- Terraform/DynamoDb.tf
+++ Terraform/DynamoDb.tf
@@ -1,3 +1,6 @@
+# Documentation References:
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
+
 resource "aws_dynamodb_table" "basic_dynamodb_table" {
   name           = var.tableName
   billing_mode   = "PAY_PER_REQUEST"
