f88d2b2a622ed582a0cf37b7999ec737d9609533
Lucian Cibu <luciancibu@yahoo.com>
2025-12-07 01:25:25 +0200

    Lambda function + IAM role using Terraform

---
+++ Terraform/Iam.tf
@@ -0,0 +1,42 @@
+# Documentation References:
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
+
+resource "aws_iam_role" "resume_lucian_cibu_lambda_role_terraform" {
+  name = "resume-lucian-cibu-lambda-role-terraform"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "resume_lucian_cibu_lambda_policy_terraform" {
+  name = "resume-lucian-cibu-lambda-policy-terraform"
+  role = aws_iam_role.resume_lucian_cibu_lambda_role_terraform.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem"
+        ],
+        Resource = "arn:aws:dynamodb:us-east-1:*:table/resume-lucian-cibu"
+      }
+    ]
+  })
+}
+
+
+
---
+++ Terraform/Lambda.tf
@@ -0,0 +1,28 @@
+# Documentation References:
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
+# https://medium.com/@haissamhammoudfawaz/create-a-aws-lambda-function-using-terraform-and-python-4e0c2816753a
+
+data "archive_file" "python_lambda_package" {
+  type        = "zip"
+  source_file = "../Lambda/lambda_function.py"
+  output_path = "${path.module}/Lambda.zip"
+}
+
+resource "aws_lambda_function" "resume_lambda" {
+  function_name = "lucian-cibu-resume-lambda-terraform"
+
+  handler = "lambda_function.lambda_handler"
+  runtime = "python3.12"
+
+  role = aws_iam_role.resume_lucian_cibu_lambda_role_terraform.arn
+
+  filename         = data.archive_file.python_lambda_package.output_path
+  source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
+
+  timeout = 5
+}
+
+resource "aws_lambda_function_url" "example" {
+  function_name      = aws_lambda_function.resume_lambda.function_name
+  authorization_type = "NONE"
+}
