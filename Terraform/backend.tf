7cab6e6910e970ff8e9b151ddd8e215a242325ef
Lucian Cibu <luciancibu@yahoo.com>
2026-01-03 15:49:48 +0200

    Enable DynamoDB state locking

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,21 @@
 terraform {
   backend "s3" {
-    bucket = "s3-tfstate-083971419667"
-    key    = "resume/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "s3-tfstate-083971419667"
+    key            = "resume/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
   }
 }
+# terraform init -migrate-state
+
+# Create dynamo db table for lock
+# aws dynamodb create-table \
+#   --table-name terraform-locks \
+#   --attribute-definitions AttributeName=LockID,AttributeType=S \
+#   --key-schema AttributeName=LockID,KeyType=HASH \
+#   --billing-mode PAY_PER_REQUEST \
+#   --region us-east-1
+
+# Check:
+# aws dynamodb describe-table --table-name terraform-locks --region us-east-1
+
